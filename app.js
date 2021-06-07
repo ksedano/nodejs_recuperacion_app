367821c213d1806f288feca3a731dc806950103e
DESKTOP-GP18RMF\Kevin <kevinsedanosmx@gmail.com>
2021-06-07 19:58:38 +0200

    api token

--- app.js
+++ app.js
@@ -4,27 +4,48 @@ const port = 3000
 
 var bodyParser = require('body-parser')
 const { response } = require('express')
-app.use(bodyParser.urlencoded({ extended: false }))
-
 var LocalStorage = require('node-localstorage').LocalStorage;
 localStorage = new LocalStorage('./scratch');
-localStorage.setItem("username","Kevin");
-localStorage.setItem("password","1234");
+
+const keyLocalStorage = "users";
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+let users = {}
+
+const user = { 
+    "password" : "1234", 
+    "token" : "token"
+}
+users["Kevin"] = user;
+
+localStorage.setItem(keyLocalStorage,JSON.stringify(users));
 
 app.get('/', (req, res) => {
-    res.redirect("/login");})
+    res.redirect("/login");
+})
 
 app.post('/home', (req, res) => {
     const username = req.body.nombre;
     const password = req.body.password;
 
-    if (localStorage.getItem("username") == username) {
-        if (localStorage.getItem("password") == password) {
-            res.send(`Hello ${req.body.nombre}!`)
-        }
-        res.send("La contraseña no coincide.")
+    const users = JSON.parse(localStorage.getItem(keyLocalStorage));
+    const user = users[username];
+
+    if (user == null) {
+        return res.send("El usuario no existe.")
     }
-    res.send("El usuario no existe.")
+
+    if(!user.token){
+        return res.send("No existe token.")
+    }
+   
+    if (user.password != password) {
+       return res.send("La contraseña no coincide.")
+    }
+
+    res.send(`<html><body><h3>Hello ${username}!</h3><button type="text"><a href="/logout?username=${username}">Cerrar session</a></button></body></html>`)
+    
   })
 
 app.get('/login', (req, res) => {
@@ -38,16 +59,43 @@ app.get('/register', (req, res) => {
 app.post('/add', (req, res) => {
     var username = req.body.nombre;
     var password = req.body.password;
+    const users = JSON.parse(localStorage.getItem(keyLocalStorage));
+
     if (username == "" || username == " ") {
         res.send("El usuario no puede estar vacio.")
     }
     if (password == "" || password == " ") {
         res.send("La contraseña no puede estar vacia.")
     }
-    localStorage.setItem("username",username)
-    localStorage.setItem("password",password)
+
+    const newUser = { 
+        "password" : password, 
+        "token" : "token"
+    }
+    users[username] = newUser;
+    localStorage.setItem("users",JSON.stringify(users));
+
 
     res.redirect("/login");
+})
+
+app.get('/logout', (req, res) => {
+    const username = req.query.username
+    const users = JSON.parse(localStorage.getItem(keyLocalStorage));
+    const user = users[username]
+
+    if(user == null){
+        return res.send("El usuario no existe")
+    }
+
+    user.token = "";
+    users[username] = user;
+
+    console.log(users);
+
+    localStorage.setItem("users", JSON.stringify(users));
+
+    res.redirect("/login")
 })
 
 app.listen(port, () => {
